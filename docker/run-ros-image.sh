1e5fcff367ce2d9ce442b7e46fa2a966e5fd8e2e
Pau Carre Cardona <pau.carre@cm-robotics.com>
2021-05-23 09:12:43 +0200

    Run docker image with ros user

--- docker/run-ros-image.sh
+++ docker/run-ros-image.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker run -it tsdf-plusplus-ros-catkin-build:v0.1 bash
+docker container run --rm -it \
+  --user 1001 \
+  --workdir /home/ros/catkin_ws \
+  tsdf-plusplus-ros-catkin-build:v0.1 \
+  bash
